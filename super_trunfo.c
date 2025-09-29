a1236b703591bee15cd856fb4e04868a9b96272a
gac726 <gcarraro258@gmail.com>
2025-09-29 17:51:25 -0300

    Create super_trunfo.c

---
+++ super_trunfo.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+
+int main(){
+    int Carta;
+    char Estado[50], Código[50], NomeDaCidade[50];
+    int População, NúmeroDePontosTuristicos;
+    float Área, PIB;
+
+    printf ("Carta: \n");
+    scanf("%d", &Carta);
+
+    printf ("Estado: \n");
+    scanf ("%s", &Estado );
+
+    printf ("Código:\n" );
+    scanf ("%s" , &Código);
+
+    printf ("NomeDaCidade: \n");
+    scanf ("%s" ,&NomeDaCidade );
+
+    printf ("População: \n" );
+    scanf ("%d" , &População);
+
+    printf ("NúmeroDePontosTuristicos: \n");
+    scanf("%d", &NúmeroDePontosTuristicos);
+
+    printf ("Área : \n");
+    scanf (" %f" , &Área);
+
+    printf ("PIB : \n");
+    scanf (" %f" ,&PIB);
+
+printf ("Estado: %s - Código: %s - NomeDaCidade: %s \n" , Estado, Código, NomeDaCidade);
+printf ("População: %d - NúmeroDePontosTuristicos: %d \n",População, NúmeroDePontosTuristicos);
+printf ("Área: %f - PIB: %f \n ",Área,PIB);
+
+#include <stdio.h>
+
+int main(){
+    int Carta;
+    char Estado[50], Código[50], NomeDaCidade[50];
+    int População, NúmeroDePontosTuristicos;
+    float Área, PIB;
+
+    printf ("Carta: \n");
+    scanf("%d", &Carta);
+
+    printf ("Estado: \n");
+    scanf ("%s", &Estado );
+
+    printf ("Código:\n" );
+    scanf ("%s" , &Código);
+
+    printf ("NomeDaCidade: \n");
+    scanf ("%s" ,&NomeDaCidade );
+
+    printf ("População: \n" );
+    scanf ("%d" , &População);
+
+    printf ("NúmeroDePontosTuristicos: \n");
+    scanf("%d", &NúmeroDePontosTuristicos);
+
+    printf ("Área : \n");
+    scanf (" %f" , &Área);
+
+    printf ("PIB : \n");
+    scanf (" %f" ,&PIB);
+
+printf ("Estado: %s - Código: %s - NomeDaCidade: %s \n" , Estado, Código, NomeDaCidade);
+printf ("População: %d - NúmeroDePontosTuristicos: %d \n",População, NúmeroDePontosTuristicos);
+printf ("Área: %f - PIB: %f \n ",Área,PIB);
+
+return 0;
+
+}
